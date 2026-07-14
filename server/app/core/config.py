bb0e99439f9ec3bb900b9f1d73a381dad07f56b1
SarayutSupsin <aidkung555@gmail.com>
2026-07-14 13:55:28 +0700

    test pull

--- server/app/core/config.py
+++ server/app/core/config.py
@@ -1,14 +1,33 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
+import os
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = "Deep Learning-Based System for Segmentation and Monitoring of Diabetic Foot Ulcer"
     API_V1_STR: str = "/api/v1"
     DATABASE_URL: str
 
+
+    BASE_DIR: str = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+    MODEL_PATH: str = os.path.join(BASE_DIR, "weights", "unet_efficientnet_b4_dfu.pth")
+    STATIC_DIR: str = os.path.join(BASE_DIR, "static", "wounds")
+    
+    #DFU Scale Parameters
+    QR_SIZE_CM: float = 2.0
+    TARGET_PX_PER_CM: float = 100.0 # 1 cm = 100 px
+    MAX_WARPED_DIM: int = 5000
+    THRESHOLD: float = 0.5
+
+    @property
+    def DEVICE(self) -> str:
+        import torch
+        return "cuda" if torch.cuda.is_available() else "cpu"
+
+    #Database models
     model_config = SettingsConfigDict(
         env_file="../.env",
         env_file_encoding="utf-8",
         extra="ignore"
     )
+    
 
 settings = Settings()
--- server/main.py
+++ server/main.py
@@ -1,12 +1,20 @@
 from fastapi import FastAPI, Depends
+from fastapi.staticfiles import StaticFiles
 from sqlalchemy.orm import Session
 from typing import List
+import os
 
 from app.core.config import settings
 from app.db.session import SessionLocal
 from app.models import Role, User, Nurse, Patient, BodyPart, Wound, WoundRecord, Appointment
+from services.
+
+
+os.makedirs(settings.STATIC_DIR, exist_ok=True)
 
 app = FastAPI(title=settings.PROJECT_NAME)
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 def get_db():
     db = SessionLocal()
@@ -23,4 +31,4 @@ def read_root():
 def test_db_connection(db:Session=Depends(get_db)):
     parts = db.query(BodyPart).all()
     return [{"id": p.body_part_id, "name": p.body_part_name} for p in parts]
-    
+
